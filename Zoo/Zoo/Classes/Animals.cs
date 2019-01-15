47032e7a2aee159714a2f9df377426033d4f57a2
mbgoseco <mbgoseco@gmail.com>
2019-01-15 03:27:44 -0800

    Animals class set up

---
+++ Zoo/Zoo/Classes/Animals.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo.Classes
+{
+    abstract public class Animals
+    {
+        abstract public int MaxGuests { get; set; }
+        abstract public int AnimalCount { get; set; }
+        abstract public bool IsWarmBlooded { get; set; }
+        virtual public bool LaysEggs { get; set; }
+        virtual public bool IsVenomous { get; set; }
+        virtual public string OpenHours { get; set; } = "9am - 8pm";
+
+        abstract public string Speak();
+        abstract public string CleanCage();
+
+        virtual public string StaffFeedAnimal()
+        {
+            return "The employees are off to feed the animals.";
+        }
+
+    }
+}
